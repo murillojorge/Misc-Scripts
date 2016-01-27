49bb8b51e3208585782f240dbe1074edfdc0658b
Jorge Murillo <murillojorge@gmail.com>
2016-01-27 14:50:33 -0600

    with an alert

--- message-until-date.php
+++ message-until-date.php
@@ -1,9 +1,11 @@
 <?php
-
 $today = date("Y-m-d H:i:s");
 $date = "2015-12-10 03:00:00";
 if ( strtotime($date) > strtotime($today) ) {
-	echo "Done!";
+	echo "
+	<script type="text/javascript">
+		alert('done!');
+	</script>
+	";
 }
-
 ?>
